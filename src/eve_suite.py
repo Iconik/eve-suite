c083693f7a16825c2241743ad7e2e931cd04aaa2
FrederikNS <frederikns@gmail.com>
2011-03-18 00:05:45 +0100

    Reinstated MainWindow, with a single icon

--- src/eve_suite.py
+++ src/eve_suite.py
@@ -6,10 +6,11 @@ Created on Nov 30, 2009
 import sys
 from PySide import QtCore, QtGui
 from view.blueprintcalculator.blueprint_calculator_view import BlueprintCalculatorView
+from view.mainwindow.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    mainwindow = BlueprintCalculatorView()
+    mainwindow = MainWindow()
     mainwindow.show()
     
     sys.exit(app.exec_())
--- src/view/mainwindow/main_window.py
+++ src/view/mainwindow/main_window.py
@@ -3,19 +3,19 @@ Created on Oct 28, 2009
 
 @author: frederikns
 '''
-import wx
-from wx import xrc
+from PySide import QtGui, QtCore
 from view.blueprintcalculator.blueprint_calculator_view import BlueprintCalculatorView
+from view.mainwindow.ui_main_window import Ui_MainWindow
 
-class MainWindow: #IGNORE:R0904
-    def __init__(self):
-        self.res = xrc.XmlResource('view/mainwindow/main_window.xrc')
-        self.init_frame()
-    
-    def init_frame(self):
-        self.frame = self.res.LoadFrame(None, 'main_frame')
-        self.frame.Bind(wx.EVT_BUTTON, self.blueprint_calc, id=xrc.XRCID('blueprint_calculator_button'))
-        self.frame.Show()
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        #Initialization
+        QtGui.QMainWindow.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         
-    def blueprint_calc(self, event):
+        self.ui.manufacturing_calculator_button.clicked.connect(self.blueprint_calc)
+        
+    def blueprint_calc(self):
         bpc = BlueprintCalculatorView(self)
+        bpc.show()
